3051dcc57c33677e0a2f09ab751eb08ddf47f14c
lifnaja <alif@prontomarketing.com>
2018-12-10 10:23:46 +0700

    Fix entrypoint.sh

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -2,4 +2,4 @@
 
 cd simplezat
 pipenv run python manage.py migrate --settings=simplezat.settings.dev
-pipenv run python manage.py runserver 0.0.0.0:8000 migrate --settings=simplezat.settings.dev
+pipenv run python manage.py runserver 0.0.0.0:8000 --settings=simplezat.settings.dev
--- entrypoint.production.sh
+++ entrypoint.production.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd simplezat
-pipenv run python manage.py migrate migrate --settings=simplezat.settings.prod
+pipenv run python manage.py migrate --settings=simplezat.settings.prod
 pipenv run python manage.py runserver 0.0.0.0:8000 --settings=simplezat.settings.prod
