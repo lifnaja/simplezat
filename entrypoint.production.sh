63b9f2737543f5edb28638a87619e59c1b3fd1a0
lifnaja <alif@prontomarketing.com>
2018-12-10 20:34:58 +0700

    hellooooo

--- entrypoint.production.sh
+++ entrypoint.production.sh
@@ -3,4 +3,4 @@
 cd simplezat
 pipenv run python manage.py migrate --settings=simplezat.settings.prod
 pipenv run python manage.py collectstatic --noinput --settings=simplezat.settings.prod
-pipenv run uwsgi --ini uwsgi.pord.ini
+pipenv run uwsgi --ini uwsgi.prod.ini
