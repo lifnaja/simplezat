fd91bd531dbec240e385e4ef2ebba11f5735bda6
lifnaja <alif@prontomarketing.com>
2018-12-10 18:36:42 +0700

    Fix entrypoint

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -3,4 +3,4 @@
 cd simplezat
 pipenv run python manage.py migrate --settings=simplezat.settings.dev
 pipenv run python manage.py collectstatic --noinput --settings=simplezat.settings.dev
-pipenv run run uwsgi --ini uwsgi.dev.ini
+pipenv run uwsgi --ini uwsgi.dev.ini
