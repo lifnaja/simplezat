bcb805269c131aebb0be8d70706c56fd73c0d242
lifnaja <alif@prontomarketing.com>
2018-12-10 10:15:56 +0700

    Edit entrypoints

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd simplezat
-pipenv run python manage.py migrate --setting=simplezat.setting.dev
-pipenv run python manage.py runserver 0.0.0.0:8000 migrate --setting=simplezat.setting.dev
+pipenv run python manage.py migrate --settings=simplezat.settings.dev
+pipenv run python manage.py runserver 0.0.0.0:8000 migrate --settings=simplezat.settings.dev
--- entrypoint.production.sh
+++ entrypoint.production.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd simplezat
-pipenv run python manage.py migrate migrate --setting=simplezat.setting.prod
-pipenv run python manage.py runserver 0.0.0.0:8000 --setting=simplezat.setting.prod
+pipenv run python manage.py migrate migrate --settings=simplezat.settings.prod
+pipenv run python manage.py runserver 0.0.0.0:8000 --settings=simplezat.settings.prod
